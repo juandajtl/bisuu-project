41046df8fb8628b66a11bca67fc03ed9218d23a4
Juan Manuel <justin4949ch@gmail.com>
2023-02-10 14:17:14 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import engine from 'ejs-mate';
+import path  from 'path';
+
+
+
+
+// settings
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 4002);
+
+//starting the server
+app.listen(app.get('port'),() => {
+
+console.log('server Running', app.get('port'));
+});
---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/' , (req, res,next) => {
+
+});
+route.get('/signup' , (req, res,next) => {
+
+});
+
+route.post('/signup' , (req, res,next) => {
+
+});
+
+route.get('/signin' , (req, res,next) => {
+
+});
+
+route.post('/signin' , (req, res,next) => {
+
+});
+
+module.exports = router;
