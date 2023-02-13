af699fc43d6e1f4526925be36b3a29d45befbd8e
Juan Manuel <justin4949ch@gmail.com>
2023-02-13 16:52:58 -0500

    nada

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import express from 'express';
 import engine from 'ejs-mate';
 import path  from 'path';
 
-
-
+const app = express();
 
 // settings
-const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
@@ -14,6 +12,5 @@ app.set('port', process.env.PORT || 4002);
 
 //starting the server
 app.listen(app.get('port'),() => {
-
 console.log('server Running', app.get('port'));
 });
